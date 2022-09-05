ec855bb3fecb6c48fe6aa1f533708aeebfa1f34d
ayano309 <ayatomato.k@gmail.com>
2022-09-05 10:08:29 +0900

    case文

--- 2-4.sql
+++ 2-4.sql
@@ -4,3 +4,4 @@ INSERT INTO 口座(口座番号,名義,種別,残高,更新日) VALUES ('0652281
 UPDATE 口座 SET 残高=(残高-3000)*1.003 WHERE 口座番号 IN ('0652281','1026413','2239710')
 SELECT 口座番号,更新日,更新日+180 AS 通帳期限日 FROM 口座 WHEREE 更新日 < '2021-01-01'
 SELECT 口座番号,'カ)' || 名義 FROM 口座 WHERE 種別 = '3'
+SELECT DISTINCT  種別 AS 種別コード, CASE 種別 WHEN '1' THEN '普通' WHEN '2' THEN '当座' WHEN '3' THEN '別段' END AS 種別名 FROM 口座
